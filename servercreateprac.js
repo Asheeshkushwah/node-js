6027111a02e8bf99d1032758a59d45b8a382ff78
asheesh kushwah <asheesh94065@gmail.com>
2024-08-01 08:47:55 +0530

    tryhgj

--- servercreateprac.js
+++ servercreateprac.js
@@ -3,6 +3,6 @@ const h = require('http')
 
 
 h.servercreate((req, res) => {
-    res.write('welcoce node js234567iw3456789')
+    res.write('welcoce node js234567iw3456789gfch54ertyhhh')
     res.end()
 }).listen((3000, () => console.log("server start localhost:3000")))
